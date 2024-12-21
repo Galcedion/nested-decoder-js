775882c9bf7ac42c7735d7aa6a92ee29e0b5eae3
Galcedion <galcedion@gmx.de>
2024-12-21 17:56:49 +0100

    adding several named numeral systems

--- nested-decoder.js
+++ nested-decoder.js
@@ -32,7 +32,48 @@ function nestedDecoderDecode(encodedString, pattern) {
 				decodedString = nestedDecoderDecodeBase64(decodedString);
 				break;
 			case 'binary':
-				decodedString = nestedDecoderDecodeBinary(decodedString);
+			case 'base2':
+				decodedString = nestedDecoderDecodeMultiBase(decodedString, 2);
+				break;
+			case 'duodec':
+			case 'base12':
+				decodedString = nestedDecoderDecodeMultiBase(decodedString, 12);
+				break;
+			case 'hex':
+			case 'base16':
+				decodedString = nestedDecoderDecodeMultiBase(decodedString, 16);
+				break;
+			case 'oct':
+			case 'base8':
+				decodedString = nestedDecoderDecodeMultiBase(decodedString, 8);
+				break;
+			case 'pental':
+			case 'base5':
+				decodedString = nestedDecoderDecodeMultiBase(decodedString, 5);
+				break;
+			case 'quaternary':
+			case 'base4':
+				decodedString = nestedDecoderDecodeMultiBase(decodedString, 4);
+				break;
+			case 'senary':
+			case 'base6':
+				decodedString = nestedDecoderDecodeMultiBase(decodedString, 6);
+				break;
+			case 'septenary':
+			case 'base7':
+				decodedString = nestedDecoderDecodeMultiBase(decodedString, 7);
+				break;
+			case 'trinary':
+			case 'base3':
+				decodedString = nestedDecoderDecodeMultiBase(decodedString, 3);
+				break;
+			case 'unary':
+			case 'base1':
+				decodedString = nestedDecoderDecodeUnary(decodedString);
+				break;
+			case 'vigesimal':
+			case 'base20':
+				decodedString = nestedDecoderDecodeMultiBase(decodedString, 20);
 				break;
 		}
 	}
@@ -47,11 +88,20 @@ function nestedDecoderDecodeBase64(encodedString) {
 	return atob(encodedString);
 }
 
-function nestedDecoderDecodeBinary(encodedString) {
+function nestedDecoderDecodeMultiBase(encodedString, base) {
 	encodedString = encodedString.split(' ');
 	var decodedString = '';
 	for(let i = 0; i < encodedString.length; i++) {
-		decodedString += parseInt(encodedString[i], 2).toString() + ' ';
+		decodedString += parseInt(encodedString[i], base).toString() + ' ';
+	}
+	return decodedString.slice(0, -1);
+}
+
+function nestedDecoderDecodeUnary(encodedString) {
+	encodedString = encodedString.split(' ');
+	var decodedString = '';
+	for(let i = 0; i < encodedString.length; i++) {
+		decodedString += encodedString[i].length + ' ';
 	}
 	return decodedString.slice(0, -1);
 }
