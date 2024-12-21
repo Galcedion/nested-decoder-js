8062ce812954421ab168bda2f56f78d155db228f
Galcedion <galcedion@gmx.de>
2024-12-21 16:54:18 +0100

    adding support for ascii and binary

--- nested-decoder.js
+++ nested-decoder.js
@@ -25,14 +25,33 @@ function nestedDecoderDecode(encodedString, pattern) {
 	var decodedString = encodedString;
 	for(let i = 0; i < pattern.length; i++) {
 		switch(pattern[i]) {
+			case 'ascii':
+				decodedString = nestedDecoderDecodeAscii(decodedString);
+				break
 			case 'base64':
 				decodedString = nestedDecoderDecodeBase64(decodedString);
+				break;
+			case 'binary':
+				decodedString = nestedDecoderDecodeBinary(decodedString);
 				break;
 		}
 	}
 	return decodedString;
 }
 
+function nestedDecoderDecodeAscii(encodedString) {
+	return String.fromCharCode.apply(null, encodedString.split(' '));
+}
+
 function nestedDecoderDecodeBase64(encodedString) {
 	return atob(encodedString);
+}
+
+function nestedDecoderDecodeBinary(encodedString) {
+	encodedString = encodedString.split(' ');
+	var decodedString = '';
+	for(let i = 0; i < encodedString.length; i++) {
+		decodedString += parseInt(encodedString[i], 2).toString() + ' ';
+	}
+	return decodedString.slice(0, -1);
 }
